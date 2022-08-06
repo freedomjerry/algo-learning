6714b57541b2a25e7e2e6486013cc2ea1ef9bccc
hotjerry <254406949@qq.com>
2022-08-06 13:31:06 +0800

    add 101

---
+++ LeetCode/101.对称二叉树/101.对称二叉树.go
@@ -0,0 +1,23 @@
+package main
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return do(root.Left, root.Right)
+}
+func do(l *TreeNode,  r *TreeNode) bool {
+	if l == nil && r == nil {
+		return true
+	}
+	if l == nil || r == nil {
+		return false
+	}
+	return l.Val == r.Val && do(l.Left, r.Right) && do(r.Left, l.Right)
+}
